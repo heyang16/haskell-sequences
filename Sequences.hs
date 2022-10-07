b3bfabbc14de1594e5804bcb45da47e6136b2634
Heyang <heyang16@users.noreply.github.com>
2022-10-07 14:45:47 +0100

    Finished first part

--- Sequences.hs
+++ Sequences.hs
@@ -5,40 +5,39 @@ import Data.Char (ord, chr)
 -- Returns the first argument if it is larger than the second,
 -- the second argument otherwise
 maxOf2 :: Int -> Int -> Int
-maxOf2 
-  = undefined
+maxOf2 x y
+  = if x > y then x else y
 
 -- Returns the largest of three Ints
 maxOf3 :: Int -> Int -> Int -> Int
-maxOf3
-  = undefined
+maxOf3 x y z = maxOf2 x (maxOf2 y z)
 
 -- Returns True if the character represents a digit '0'..'9';
 -- False otherwise
 isADigit :: Char -> Bool
-isADigit
-  = undefined
+isADigit x = 48 <= ord x && ord x <= 57
+
 
 -- Returns True if the character represents an alphabetic
 -- character either in the range 'a'..'z' or in the range 'A'..'Z';
 -- False otherwise
 isAlpha :: Char -> Bool
-isAlpha
-  = undefined
+isAlpha x 
+  = (97 <= ord x && ord x <= 122) || (65 <= ord x && ord x <= 90)
 
 -- Returns the integer [0..9] corresponding to the given character.
 -- Note: this is a simpler version of digitToInt in module Data.Char,
 -- which does not assume the precondition.
 digitToInt :: Char -> Int
 -- Pre: the character is one of '0'..'9'
-digitToInt
-  = undefined
+digitToInt x
+  = ord x - 48
 
 -- Returns the upper case character corresponding to the input.
 -- Uses guards by way of variety.
 toUpper :: Char -> Char
-toUpper 
-  = undefined
+toUpper x
+  = chr (ord x - 32)
 
 --
 -- Sequences and series
